974a59ae9f857dd8b7e367a472e29d331e727644
alsakif <alsakif1.618@gmail.com>
2022-11-03 09:52:18 +0600

    golang basic

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+	var bookedTickets uint
+	var bookings []string
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	start := time.Now()
+	for remainingTickets > 0 {
+		var firstName string
+		var lastName string
+		var email string
+		var maxTickets uint = 45
+
+		fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+		fmt.Println("Get your tickets here to attend the conference!")
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		regexPattern := regexp.MustCompile(`^[a-zA-Z]{3,10}$`).MatchString(firstName)
+
+		if !regexPattern {
+			fmt.Println("Names must be only letters!")
+			continue
+		}
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		lastNameRegexPattern := regexp.MustCompile(`^[a-zA-Z]{3,10}$`).MatchString(lastName)
+
+		if !lastNameRegexPattern {
+			fmt.Println("Names must be only letters!")
+			continue
+		}
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		emailRegexPattern := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`).MatchString(email)
+
+		if !emailRegexPattern {
+			fmt.Println("Invalid Email Address!")
+			continue
+		}
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&bookedTickets)
+
+		for bookedTickets == 0 {
+			fmt.Println("Invalid Input!")
+
+			fmt.Println("Enter number of tickets: ")
+			fmt.Scan(&bookedTickets)
+		}
+		for bookedTickets > remainingTickets {
+			fmt.Println(remainingTickets, "tickets are available. You have asked too much!")
+
+			fmt.Println("Enter number of tickets: ")
+			fmt.Scan(&bookedTickets)
+		}
+
+		for bookedTickets > maxTickets {
+			fmt.Println("You can not buy more than ", maxTickets, "tickets")
+
+			fmt.Println("Enter number of tickets: ")
+			fmt.Scan(&bookedTickets)
+		}
+
+		remainingTickets = remainingTickets - bookedTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v ticket/s. You will receive a confirmation email at %v.\n", firstName, lastName, bookedTickets, email)
+		fmt.Println(remainingTickets, "tickets are left for", conferenceName)
+
+		firstNames := []string{}
+		lastNames := []string{}
+		for _, booking := range bookings {
+			var name = strings.Fields(booking)
+			firstNames = append(firstNames, name[0])
+			lastNames = append(lastNames, name[1])
+		}
+		fmt.Println("Confirmed Bookings", firstNames)
+		fmt.Println("Last Names: ", lastNames)
+	}
+	fmt.Println("All Tickets are booked. Thank you for your concern. Please comeback in next season.")
+	duration := time.Since(start)
+	fmt.Println(duration.Milliseconds())
+}
